a299fd571b4f52dc8ec01b47feee635a53450ed1
NilsFriman <github@nils.friman.name>
2023-05-12 09:46:01 +0200

    Cleaned up.

--- client.py
+++ client.py
@@ -2,26 +2,25 @@ import socket
 import threading
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(("192.168.0.83", 1234))
+socket.connect(("192.168.0.83", 1234))
 
 
-
-def reciever():
+def receiver():
     while True:
-        msg = s.recv(1024)
+        msg = socket.recv(1024)
         print(msg.decode("utf-8"))
+
 
 def writer():
     while True:
         message = input("")
-        s.send(message.encode())
+        socket.send(message.encode())
 
 
-recieve_thread = threading.Thread(target=reciever)
+recieve_thread = threading.Thread(target=receiver)
 recieve_thread.start()
-
 
 writer_thread = threading.Thread(target=writer)
 writer_thread.start()
