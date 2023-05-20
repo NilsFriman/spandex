3f9e6d171a6b20e7e5b16fdcc5b349a3d7b078c2
litunaa <nubtem1337@gmail.com>
2023-05-20 20:23:58 +0200

    added gui for the chat and a login/create account page

--- client.py
+++ client.py
@@ -1,29 +1,241 @@
 import socket
 import threading
-
+import customtkinter
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-socket.connect(("10.158.18.131", 1234))
-
-
-def receiver():
-    while True:
-        msg = socket.recv(1024)
-        print(msg.decode("utf-8"))
-
-
-def writer():
-    while True:
-        message = input("")
-        socket.send(message.encode())
-
-
-recieve_thread = threading.Thread(target=receiver)
-recieve_thread.start()
-
-writer_thread = threading.Thread(target=writer)
-writer_thread.start()
-
+socket.connect(("192.168.0.83", 1234))
 
 # Note: Client will receive ConnectionResetError when server shuts down
+
+available_commands = {
+    "/nick": "/nick {desired nickname}",
+    "/admin": "/admin",
+    "/clear": "/clear {amount of lines}",
+    "/whisper": "/whisper {username} {message}"
+}
+
+
+class ChatLoginGUI(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self._set_appearance_mode("dark")
+        self.resizable(False, False)
+
+        self.create_main_frame()
+        self.create_login_frame()
+
+    def create_main_frame(self):
+        self.main_frame = customtkinter.CTkFrame(
+                                                self,
+                                                width=500,
+                                                height=584,
+                                                corner_radius=5,
+                                                fg_color="#1D1E1E"
+                                                )
+        self.main_frame.pack()
+        self.main_frame.pack_propagate(False)
+        self.main_frame.grid_propagate(False)
+
+    def create_login_frame(self):
+        self.login_frame = customtkinter.CTkFrame(
+                                                self.main_frame,
+                                                width=300,
+                                                height=350,
+                                                corner_radius=10,
+                                                border_width=1,
+                                                border_color="Red"
+                                                )
+        self.login_frame.pack(expand=True)
+        self.login_frame.grid_propagate(False)
+
+        self.login_label = customtkinter.CTkLabel(
+                                                self.login_frame,
+                                                text="Login",
+                                                font=("Arial", 20),
+                                                text_color="Yellow"
+                                                )
+        self.login_label.grid(row=0, column=0, pady=(25, 0))
+
+        self.username_entry = customtkinter.CTkEntry(
+                                                    self.login_frame,
+                                                    width=200,
+                                                    height=40,
+                                                    corner_radius=8,
+                                                    border_width=1,
+                                                    placeholder_text="Username"
+                                                    )
+        self.username_entry.grid(row=1, column=0, pady=(35, 0), padx=50)
+
+        self.password_entry = customtkinter.CTkEntry(
+                                                    self.login_frame,
+                                                    width=200,
+                                                    height=40,
+                                                    corner_radius=8,
+                                                    border_width=1,
+                                                    placeholder_text="Password"
+                                                    )
+        self.password_entry.grid(row=2, column=0, pady=(15, 0), padx=50)
+
+        self.error_message = customtkinter.CTkLabel(
+                                                self.login_frame,
+                                                text="",
+                                                text_color="red"
+                                                )
+        self.error_message.grid(row=3, column=0, pady=(5, 0))
+
+        self.apply_info = customtkinter.CTkButton(
+                                                self.login_frame,
+                                                width=100,
+                                                height=30,
+                                                corner_radius=8,
+                                                border_width=1,
+                                                text="Login",
+                                                command=self.login_or_create
+                                                )
+        self.apply_info.grid(row=4, column=0)
+
+        self.create = customtkinter.CTkButton(
+                                            self.login_frame,
+                                            text="Don't have an account? Click here!",
+                                            fg_color="gray17",
+                                            bg_color="gray17",
+                                            command=self.create_account
+                                            )
+        self.create.grid(row=5, column=0, pady=(10, 0))
+
+    def update_gui(self, create_text, login_label_text, apply_info_text):
+        self.create.configure(text=create_text)
+        self.login_label.configure(text=login_label_text)
+        self.apply_info.configure(text=apply_info_text)
+
+
+    def login_or_create(self):
+        self.error_message.configure(text="")
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        if not username and not password:
+            self.error_message.configure(text="Please enter a username and password!")
+        elif not username:
+            self.error_message.configure(text="Please enter a username!")
+        elif not password:
+            self.error_message.configure(text="Please enter a password!")
+        elif len(password) < 4:
+            self.error_message.configure(text="Make sure the password is longer than 3 characters!")
+        else:
+            self.username_entry.delete(0, "end")
+            self.password_entry.delete(0, "end")
+            action = "login" if self.apply_info._text == "Login" else "create"
+            socket.send(f"{username} {password} {action}".encode("utf-8"))
+            response = socket.recv(1024).decode("utf-8")
+
+            if action == "login":
+                if response == "Denied":
+                    self.error_message.configure(text="Wrong username or password!")
+                else:
+                    self.enter_chat()
+            elif response == "username not available":
+                self.error_message.configure(text="Username already taken")
+            else:
+                self.error_message.configure(
+                    text="Account created! You can now login!",
+                    text_color="lightgreen"
+                )
+
+            self.update_gui("Don't have an account? Click here!", "Login", "Login")
+
+
+    def create_account(self):
+        
+        self.error_message.configure(text="")
+        if self.create._text == "Don't have an account? Click here!":
+                    self.update_gui(
+            "Login to an existing account", "Create your account!", "Create")
+                    
+        else:
+            self.update_gui("Don't have an account? Click here!", "Login", "Login")
+
+    def enter_chat(self):
+        self.login_frame.pack_forget()
+        self.chat_gui()
+        receive_thread = threading.Thread(target=self.message_receiver)
+        receive_thread.start()
+
+    def chat_gui(self):
+        self.information_box = customtkinter.CTkFrame(
+                                                    self.main_frame,
+                                                    width=500,
+                                                    height=100,
+                                                    corner_radius=8,
+                                                    fg_color="#1D1E1E"
+                                                    )
+        self.information_box.grid(row=0, column=0)
+        self.information_box.grid_propagate(False)
+
+        self.commands = customtkinter.CTkTextbox(
+                                                self.information_box,
+                                                width=250,
+                                                height=100,
+                                                corner_radius=8,
+                                                border_width=1,
+                                                fg_color="#1D1E1E",
+                                                bg_color="#1D1E1E",
+                                                text_color="lightgreen"
+                                                )
+        self.commands.insert("0.0", "                 Available commands\n" + "\n".join(available_commands.values()))
+        self.commands.grid(row=0, column=0)
+        self.commands.configure(state="disabled")
+
+        self.online_users = customtkinter.CTkTextbox(
+                                                    self.information_box,
+                                                    width=250,
+                                                    height=100,
+                                                    corner_radius=8,
+                                                    border_width=1,
+                                                    fg_color="#1D1E1E",
+                                                    bg_color="#1D1E1E",
+                                                    text_color="lightgreen",
+                                                    state="disabled"
+                                                    )
+        self.online_users.grid(row=0, column=1)
+
+        self.chat_box = customtkinter.CTkTextbox(
+                                                self.main_frame,
+                                                width=500,
+                                                height=450,
+                                                corner_radius=8,
+                                                border_width=1,
+                                                fg_color="#1D1E1E",
+                                                text_color="lightgreen",
+                                                state="disabled"
+                                                )
+        self.chat_box.grid(row=1, column=0)
+
+        self.chat_entry = customtkinter.CTkEntry(
+                                                self.main_frame,
+                                                width=500,
+                                                height=35,
+                                                corner_radius=12,
+                                                border_width=1,
+                                                fg_color="#1D1E1E",
+                                                text_color="lightgreen",
+                                                placeholder_text="Enter a message"
+                                                )
+        self.chat_entry.grid(row=2, column=0)
+        self.bind("<Return>", self.message_sender)
+
+    def message_receiver(self):
+        while True:
+            if msg := socket.recv(1024):
+                self.chat_box.configure(state="normal")
+                self.chat_box.insert("1.0", msg.decode("utf-8") + "\n")
+                self.chat_box.configure(state="disabled")
+
+    def message_sender(self, event):
+        message = self.chat_entry.get()
+        socket.send(message.encode("utf-8"))
+        self.chat_entry.delete(0, "end")
+
+
+app = ChatLoginGUI()
+app.mainloop()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import json 
 
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,21 +10,21 @@ server_port = 1234
 try:
     server_socket.bind(("", server_port))
 except socket.error as error:
-    print(str(error))
+    print(error)
 
 server_socket.listen()
 print("Server is up and running!")
 
-
 # Clients currently connected
-# Format: {IP: {"name": name, "port": port, "connection": connection}, IP: ...}
 clients = {}
-# All clients that have been connected at some point
-# Format: {IP: {"name": name, "port": port, "connection": connection}, IP: ...}
+
+# Every client that has ever connected to the server
 users = {}
-# All names in the users list
-# Format: [name, name, name...]
+
+
+# Names of all users
 names = []
+
 # The current highest number guest
 guest_number = 0
 
@@ -32,95 +33,141 @@ commands = {
     "/admin": "admin",
     "/clear": "clear",
     "/whisper": "whisper"
-    }
+}
 
 
-def message_sender(message, specified_client=None):
-    if specified_client:  # Whisper
-        specified_client["connection"].send(message.encode())
+def message_sender(message, specified_client_connection=None):
+    if specified_client_connection:  # Whisper
+        specified_client_connection.send(message.encode())
     else:  # Public message
-        for client in list(clients.values()):
+        for client in clients.values():
             client["connection"].send(message.encode())
 
 
-def command_handler(client, command, message):
-
+def command_handler(client_username, client_info, command, message):
+    if command == "clear":
+        return
+    
     if command == "nick":
-        nickname = message.split()[1]
+        try:
+            # Lägga till att användarnamnet inte kan vara längre än typ 8 characters?
+            nickname = message[len("/nick") + 1:]
 
-        if nickname in names:
-            message_sender(f"\"{nickname}\" is already taken", client)
-        else:
-            names.append(nickname)
-            client_ip = client["connection"].getsockname()[0]
-            users[client_ip]["name"] = nickname
-            clients[client_ip]["name"] = nickname
-            message_sender(f"Your new nickname is now \"{nickname}\"")
+            if nickname in names:
+                message_sender(f"\"{nickname}\" is already taken", client_info["connection"])
+            else:
+                names.append(nickname)
+                users[client_username]["name"] = nickname
+                clients[client_username]["name"] = nickname
+
+                message_sender(f"Your new nickname is now \"{nickname}\"", client_info["connection"])
+
+        except Exception:
+            message_sender("You did not enter a nickname!", client_info["connection"])
 
     elif command == "whisper":
         recipient = message.split()[1]
         recipient_found = False
 
-        for _ , value in clients.items():
+        for value in clients.values():
             if recipient == value["name"]:
-                message_sender(f"{client['name']} whispers to you: {' '.join(message.split()[2:])}", value)
+                print(f"{client_info['name']} whispers to you: {' '.join(message.split()[2:])}")
+                message_sender(f"{client_info['name']} whispers to you: {' '.join(message.split()[2:])}", value["connection"])
                 recipient_found = True
                 break
 
         if not recipient_found:
             if recipient in (users[user]["name"] for user in users.keys()):
-                message_sender("User is currently offline", client)
+                message_sender("User is currently offline", client_info["connection"])
             else:
-                message_sender("Unrecognized user", client)
+                message_sender("Unrecognized user", client_info["connection"])
 
-    elif command == "clear":
-        pass
 
-        
+def client_handler(client_username, client_info):
+    disconnected = False
+    while not disconnected:
+        try:
+            message = client_info["connection"].recv(1024).decode()
 
-def client_handler(client):
-    try:
-        while True:
 
-                message = client["connection"].recv(1024).decode()
+            if message[0] == "/":  # Client sends a command
+                command_handler(client_username, client_info, commands[message.split()[0]], message) # (Username, client_info, command, message)
+            else:  # Client sends a normal message
+                message_sender(f"{client_info['name']}: {message}")
 
-                if message[0] == "/":  # Client sends a command
-                    command_handler(client, commands[message.split()[0]], message)
+        except ConnectionResetError:  # Client disconnected
+            disconnected_user = client_info['name']
+            clients.pop(client_username)
+            client_info["connection"].close()
+            message_sender(f"{disconnected_user} left the room")
+            disconnected = True
 
-                else:  # Client sends a normal message
-                    message_sender(f"{client['name']}: {message}")
+        # except KeyError:  # Client used a command that doesn't exist
+        #     message_sender("Invalid command!", client_info["connection"])
 
-    except ConnectionResetError:  # Client disconnected
-        disconnected_user = client['name']
-        clients.pop(client["connection"].getsockname()[0])
-        client["connection"].close()
-        message_sender(f"{disconnected_user} left the room")
 
-    except KeyError:  # Client used a command that doesn't exist
-        message_sender("Invalid command!", specified_client=client)
-            
+def login(client_address, client_connection):
+    logged_in = False
+
+    while not logged_in:
+        login_info = client_connection.recv(1024).decode("utf-8")
+
+        username = login_info.split()[0]
+        password = login_info.split()[1]
+
+        if login_info.split()[2] == "login":
+
+            if username in users.keys() and users[username]["password"] == password:
+                # Lägga till att den kollar om någon annan redan är inloggad på kontot på en annan dator? 
+                users[username]["connection"] = client_connection
+                clients[username] = users[username]
+
+                message_sender("Granted", client_connection)
+                message_sender(f"{users[username]['name']} has entered the chat!")    
+
+                thread = threading.Thread(target=client_handler, args=(username,
+                                                                        users[username]))
+                thread.start()
+                logged_in = True
+            else:
+                message_sender("Denied", client_connection)
+
+        elif username in users.keys():
+            message_sender("Username not available", client_connection)
+
+        else:
+            users[username] = {"password": password, "name": f"Guest{guest_number}", "connection": client_connection, "adress": client_address}
+            clients[username] = users[username]
+
+            message_sender("Account created", client_connection)
+
 
 def connections():
-
     global guest_number
 
     while True:
         client_connection, client_address = server_socket.accept()
-        # client_address includes many things, but client_address[0] is the IP address
-        if client_address[0] in users.keys():  # Returning user
-            users[client_address[0]]["connection"] = client_connection  # Changes client connection
-            message_sender(f'{users[client_address[0]]["name"]} has entered the chat room')
-
-        else:  # New user
-            # Creates new user in users
-            name = "Guest" + str(guest_number)
-            guest_number += 1 
-            users[client_address[0]] = {"name": name, "port": client_address[1], "connection": client_connection}
-            message_sender("Guest has entered the chat room")
-
-        clients[client_address[0]] = users[client_address[0]]
-        thread = threading.Thread(target=client_handler, args=(users[client_address[0]],))
-        thread.start()
+        login(client_address, client_connection)
 
 
+        
+    
+# Stores all the saved data of every user in local variables
+# with open("users.json", "r+", encoding="utf-8") as f:
+#     try:
+#         users_info = json.load(f)
+
+#         users = users_info
+
+#         names = [user["name"] for user in users.values()]
+
+
+#     except json.JSONDecodeError:
+#         json.dump({}, f)
+
+
+users["lituna"] = {'password': '1234', "name": "lituwu"}
+
+
+    
 connections()
